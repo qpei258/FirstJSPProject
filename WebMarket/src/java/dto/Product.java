2d4eb4e83c066d49cb85ce9bd0774f78d6f80103
qpei258 <81565492+qpei258@users.noreply.github.com>
2021-11-19 15:04:29 +0900

    Update Product.java
    cart added

--- WebMarket/src/java/dto/Product.java
+++ WebMarket/src/java/dto/Product.java
@@ -15,7 +15,7 @@ public class Product implements Serializable {
 	private long unitsInStock;		//재고 수
 	private String condition;		//신상 or 중고 or 재생품
 	private String filename;
-	
+	private int quantity;
 	
 	public Product() {
 		super();
@@ -101,6 +101,14 @@ public class Product implements Serializable {
 
 	public void setFilename(String filename) {
 		this.filename = filename;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 
 	
